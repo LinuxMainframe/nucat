14d65f60587006d7ca845eacb249a4d0dbb701e6
Brutus Gaius <aabradley01@gmail.com>
2026-07-09 00:38:13 -0400

    Enhance nucat with multi-file support and error handling
    Updated the nucat CLI tool to support multi-file output and improved error handling.

--- nucat.c
+++ nucat.c
@@ -5,73 +5,118 @@
 #include <stdio.h>
 
 
-// nucat CLI tool
-// mimicks the real CLI tool 'cat'
-// v0.0.4
+// nucat
+// A new CLI tool, not to replace cat, but to do something different
+// v0.0.5
 // Author: Aidan A. Bradley
 //
-// Only handles single file "catenation"
+// Handles single and multi-file output
+// Does not yet handle command line flags
 // usage:
-//      >> ./cat <PATH/TO/FILE>
+//      >> ./cat <PATH/TO/FILE1> <PATH/TO/FILE2> ... <PATH/TO/FILE>
 //
 // Changelog:
 //      v0.0.1 - Initial code with error handling
 //      v0.0.2 - Swapped from STDOUT to STDERR streaming
 //      v0.0.3 - Condensed if statement chain
 //      v0.0.4 - Condensed further and removed if blocks
-//
+//      v0.0.5 - Added multifile support and robust error
+//               handling
 int main(int argc, char *argv[]) {
-    // Check to see if the caller passed the PATH as an argument
+
+    // We need to keep track of the errors in the proper order
+    // so we init an array with slots for CLI argmuent added to cat
+    // hence the -1
+    int errors[argc];
+    errors[0] = 0; // Buffer the zeroth index because we capture that error separately
+    char *msg; // placeholder for our messages
+    int buffsize = 4096;
+    int errcnt = 0;
+
+    // Lets check to make sure we get more than one argument in the CLI
+    // This does not need anything special, just ensure at least one
+    // PATH is present in the command line arguments
+    // (THE COMMAND ITSELF COUNTS AS ONE ARGUMENT)
     if (argc < 2) {
-        char *msg = "PATH missing from command line arguments.\n";
-        write(2, "cat: ERROR: ", 12);
+        msg = "cat: Error: no PATH(s) supplied!\n";
         write(2, msg, strlen(msg));
         return 1;
     }
 
-    // Attempt an open() syscall
-    int fd = open(argv[1], O_RDONLY);
-    
-    // If there is an error, handle it here
-    if (fd == -1) {
+    // So we can ignore the zeroth element since the command
+    // lies in the zeroth index. And since we know that C
+    // guarantees a NULL terminator, we can use that to find the end
+    for (int i = 1; i < argc; i++) {
+        if (argv[i] == NULL) {
+            continue; // Just exit out the for loop since we found the NULL
+        } else {
+            int fd = open(argv[i], O_RDONLY); // Attempt the file opening
 
-        int op_err = errno;
-        write(2, "cat: ERROR: ", 12);
+            // If there is an error:
+            if (fd == -1) {
+                errors[i] = errno; // Store the errno in the correct index
+                errcnt++; // incrememnt error counter
+                continue; // And move on, we will print these out later
+            }
 
-        char *msg = strerror(op_err);
-        write(2, msg, strlen(msg));
-        write(2, "\n", 1);
+            // setup the storage buffer for the characters read
+            char buffer[buffsize];
+            // ensure we have a default for the amount of lines read, can change later
+            ssize_t lnread = 4096;
 
-        return 1;
-    }
+            // while we are still pulling characters out
+            while (lnread > 0) {
+                lnread = read(fd, buffer, buffsize); // store the lines read and output to buffer
 
-    // If we get a positive non-zero FD, move forwards with reading
-    char buffer[4096];
-    ssize_t lnread = 4096;
+                // Now ensure we dont accidentally get a -1 from read(), which is unsigned so becomes max size buffer
+                if (lnread == -1) {
+                    break;
+                }
+                write(1, buffer, lnread); // write buffer contents to STDOUT
+            }
 
-    while (lnread > 0) {
-        // attempt a read
-        lnread = read(fd, buffer, 4096);
-
-        // if it works, then go ahead and read until we reach EOF
-        if (lnread > 0) {
-            write(1, buffer, lnread);
+            // When we reach lnread <= 0:
+            if (lnread == 0) {
+                errors[i] = 0; // not an error
+                close(fd); // We close if the NULL terminator is found
+                continue;
+            } else if (lnread == -1) {
+                // we have reached an error
+                errors[i] = errno; // store the error
+                errcnt++; // increment error counter
+                close(fd); // close the file
+                continue; // move on
+            } else {
+                // an undefined error was caught, generate a "unknown error" message
+                msg = "cat: Error: unknown read() error!\n";
+                write(2, msg, strlen(msg));
+                errcnt++; // incrememnt error counter
+            }
         }
-    } 
-    if (lnread < 0) {
-        // otherwise we need to handle the error
-        int rd_err = errno;
-
-        char *msg = "cat: ERROR: ";
-        write(2, msg, strlen(msg));
-        msg = strerror(rd_err);
-        write(2, msg, strlen(msg));
-        write(2, "\n", 1);
-
-        close(fd);
-        return 1; // Return failure to the OS!
     }
 
-    close(fd);
-    return 0;
+    // if errors found at all
+    if (errcnt > 0) {
+        for (int i = 1; i < argc; i++) {
+            // iterate through the error messages
+            if (errors[i] == 0) {
+                continue; // dont say anything
+            } else {
+                msg = "cat: Error: ";
+                write(2, msg, strlen(msg));
+                msg = "command line argument \"";
+                write(2, msg, strlen(msg));
+                msg = argv[i];
+                write(2, msg, strlen(msg));
+                msg = "\" : ";
+                write(2, msg, strlen(msg));
+                msg = strerror(errors[i]);
+                write(2, msg, strlen(msg));
+                write(2, "\n", 1);
+            }
+        }
+        return 1; // we found at least one error, so return 1 to let the kernel know whats up
+    }
+    return 0; // otherwise, tell the kernel everything went well
+
 }
